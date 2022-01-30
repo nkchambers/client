6b7a211a7c244ca48ddcac9462376cc912891181
nkchambers <nkchames@gmail.com>
2022-01-29 19:33:56 -0800

    Added Delete Product Fucntion to Main and ViewOne

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,6 +19,23 @@ const Main = (props) => {
     }, [])
 
 
+    // DELETE PRODUCT FUNCTION
+    const deleteProduct = (deleteId) => {
+        // console.log(deleteId);
+
+        // Make Axios Call to server to instruct db to delete
+        axios.delete(`http://localhost:8000/api/products/delete/${deleteId}`)
+        .then( res => {
+            console.log(res.data);
+            console.log('PRODUCT DELETED!');
+
+            // Remove Product From DOM After delete succuss
+            setProductsData(productsData.filter( (product) => product._id !== deleteId))
+        })
+        .catch(err => console.log(err))
+    }
+
+
     return (
         <div className='form-control btn-dark'>
             <h1>All Products</h1>
@@ -27,8 +44,12 @@ const Main = (props) => {
                 // JSON.stringify(data)
                 productsData.map((product, _id) => {
                     return (
-                            <p key={product._id} style={{fontSize: "20px"}}>
-                                <Link to={ `/products/${product._id}`} style={{color: "cyan"}}>{product.title}</Link>
+                            <p key={product._id} style={{ fontSize: "22px" }}>
+                                <Link to={ `/products/${product._id}`} style={{ color: "cyan" }}>{product.title}</Link>
+
+                                <button className="btn btn-dark" onClick={ () => deleteProduct(product._id)}
+                                style={{ padding: "5px 16px", fontSize: "16px", border: "2px solid cyan", float: "right" }}>Delete</button>
+                                {/* <Link to={`/products/delete/${product._id}`} style={{color: "cyan"}}>Delete</Link></button> */}
                             </p>
                     )
                 })   
--- src/components/ViewOne.jsx
+++ src/components/ViewOne.jsx
@@ -25,18 +25,43 @@ const ViewOne = (props) => {
     }, [id])
 
 
+
+
+    // DELETE PRODUCT FUNCTION
+    const deleteProduct = (deleteId) => {
+        // console.log(deleteId);
+
+        // Make Axios Call to server to instruct db to delete
+        axios.delete(`http://localhost:8000/api/products/delete/${deleteId}`)
+        .then( res => {
+            console.log(res.data);
+            console.log('PRODUCT DELETED!');
+
+            // // Remove Product From DOM After delete succuss
+            // setProductsData(productsData.filter( (product) => product._id !== deleteId))
+        })
+        .catch(err => console.log(err))
+    }
+
+
     return (
-        <div className='form-control btn-dark' style={{margin: "20px auto", borderRadius: "5px"}}>
+        <div className='form-control btn-dark' style={{margin: "30px auto", borderRadius: "5px", 
+        paddingTop: "20px", paddingBottom: "20px"}}>
             <h2 style={{color: "cyan", padding: "20px auto"}}>{viewProduct.title}</h2>
             <hr />
             <p style={{fontSize: "20px", padding: "5px auto"}}>
                 Price: ${viewProduct.price}</p>
-            <p style={{fontSize: "20px", padding: "5px auto", marginBottom: "50px"}}>
+            <p style={{fontSize: "20px", padding: "5px auto", marginBottom: "100px"}}>
                 Description: {viewProduct.description}</p>
 
             <button className="btn btn-dark"
-            style={{ padding: "5px 20px", fontSize: "18px", border: "2px solid cyan" }}>
+            style={{ padding: "5px 20px", fontSize: "18px", border: "2px solid cyan", marginRight: "200px" }}>
             <Link to={`/products/update/${viewProduct._id}`} style={{color: "cyan"}}>Update</Link></button>
+
+            
+            <button className="btn btn-dark" onClick={ () => deleteProduct(viewProduct._id)}
+            style={{ padding: "5px 20px", fontSize: "18px", border: "2px solid cyan" }}>
+            <Link to='/' style={{color: "cyan"}}>Delete</Link></button>
         </div>
     );
 };
